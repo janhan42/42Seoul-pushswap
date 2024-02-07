addd4ada8e3a3333670b615c2cded8c3c44c6790
janhan42 <janhan@student.42seoul.kr>
2024-02-07 17:53:38 +0900

    worst case fix plz

--- push_swap_file/srcs/ft_sort_big.c
+++ push_swap_file/srcs/ft_sort_big.c
@@ -6,12 +6,29 @@
 /*   By: janhan <janhan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 10:54:03 by janhan            #+#    #+#             */
-/*   Updated: 2024/02/07 15:51:09 by janhan           ###   ########.fr       */
+/*   Updated: 2024/02/07 17:53:10 by janhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-#include <stdio.h>
+
+static void	rotation(t_stack **stack, int count_from_top, int count_from_bottom)
+{
+	if (count_from_top == 0)
+		return ;
+	if (count_from_top <= count_from_bottom)
+		while (count_from_top > 0)
+		{
+			ft_ra(stack);
+			count_from_bottom--;
+		}
+	else
+		while (count_from_bottom > 0)
+		{
+			ft_rra(stack);
+			count_from_bottom--;
+		}
+}
 
 void optimize_rotation(t_stack **stack_a, int index, int chunk)
 {
@@ -22,34 +39,22 @@ void optimize_rotation(t_stack **stack_a, int index, int chunk)
 	t_stack *current = *stack_a;
 	t_stack *tail = NULL;
 
-	// 스택의 끝(tail)을 찾기
 	while (current && current->next)
 		current = current->next;
 	tail = current;
-	// 상단에서 조건에 맞는 노드까지의 거리 계산
 	current = *stack_a;
 	while (current && !(current->index < index + chunk))
 	{
 		count_from_top++;
 		current = current->next;
 	}
-	// 하단에서 조건에 맞는 노드까지의 거리 계산
-	// current = tail;
-	// while (current && !(current->index < index + chunk))
-	// {
-	// 	count_from_bottom++;
-	// 	current = current->prev; // 이전 노드로 이동 (더블 링크드 리스트인 경우)
-	// }
-	//printf("f_top: %d , f_bottom %d \n", count_from_top, count_from_bottom);
-	if (!count_from_top)
-		return ;
-	while (count_from_top-- > 0)
-		ft_ra(stack_a);
-	// else
-	// {
-	// 	while (count_from_bottom-- > 0)
-	// 		ft_rra(stack_a);
-	// }
+	current = tail;
+	while (current && !(current->index < index + chunk))
+	{
+		count_from_bottom++;
+		current = current->prev;
+	}
+	rotation(stack_a, count_from_top, count_from_bottom);
 }
 
 t_stack	*ft_sort_b(t_stack **stack_a)
@@ -57,23 +62,23 @@ t_stack	*ft_sort_b(t_stack **stack_a)
 	t_stack	*stack_b;
 	size_t	size;
 	long	index;
-	int		count;
-	int		cunk;
+	int		chunk;
+	int		to_find;
 
 	stack_b = NULL;
 	size = ft_stacksize(*stack_a);
-	cunk = ft_get_cunk(stack_a);
+	chunk = ft_get_cunk(stack_a);
 	index = 0;
-	count = 0;
 	while (size--)
 	{
-		optimize_rotation(stack_a, index, cunk);
-		int tttt = (*stack_a)->index;
+		optimize_rotation(stack_a, index, chunk);
+		to_find = (*stack_a)->index;
 		ft_pb(stack_a, &stack_b);
-		if (count < tttt && tttt <= count + cunk)
+		if (index <= to_find && to_find <= index + chunk)
 			ft_rb(&stack_b);
+		else
+			;
 		index++;
-		count++;
 	}
 	return (stack_b);
 }
@@ -90,7 +95,7 @@ t_stack	**ft_sort_a(t_stack **stack_a, t_stack **stack_b)
 	while (*stack_b)
 	{
 		to_find = ft_find_index(*stack_b, ft_max(*stack_b), &order);
-		if (order > ft_stacksize(*stack_b) / 2)
+		if (order >= ft_stacksize(*stack_b) / 2)
 			down_pa(stack_a, stack_b, ft_find_max_addr(stack_b));
 		else
 		 	up_pa(stack_a, stack_b, ft_find_max_addr(stack_b));
@@ -115,3 +120,8 @@ void	ft_sort(t_stack **stack_a)
 		stack_a = ft_sort_a(stack_a, &stack_b);
 	}
 }
+
+/*
+./push_swap 496 497 498 499 500 501 502 503 504 505 506 507 508 509 510 511 512 513 514 515 516 517 518 519 520 521 522 523 524 525 526 465 466 467 468 469 470 471 472 473 474 475 476 477 478 479 480 481 482 483 484 485 486 487 488 489 490 491 492 493 494 495 434 435 436 437 438 439 440 441 442 443 444 445 446 447 448 449 450 451 452 453 454 455 456 457 458 459 460 461 462 463 464 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 431 432 433 372 373 374 375 376 377 378 379 380 381 382 383 384 385 386 387 388 389 390 391 392 393 394 395 396 397 398 399 400 401 402 341 342 343 344 345 346 347 348 349 350 351 352 353 354 355 356 357 358 359 360 361 362 363 364 365 366 367 368 369 370 371 310 311 312 313 314 315 316 317 318 319 320 321 322 323 324 325 326 327 328 329 330 331 332 333 334 335 336 337 338 339 340 279 280 281 282 283 284 285 286 287 288 289 290 291 292 293 294 295 296 297 298 299 300 301 302 303 304 305 306 307 308 309 248 249 250 251 252 253 254 255 256 257 258 259 260 261 262 263 264 265 266 267 268 269 270 271 272 273 274 275 276 277 278 217 218 219 220 221 222 223 224 225 226 227 228 229 230 231 232 233 234 235 236 237 238 239 240 241 242 243 244 245 246 247 186 187 188 189 190 191 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 | pbcopy
+최악 케이스
+*/
